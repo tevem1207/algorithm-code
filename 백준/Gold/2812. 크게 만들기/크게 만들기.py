623b7215cf0c10d8e1f014d72e7e5e71ec9609b5
Taehee_Hwang <89463824+tevem1207@users.noreply.github.com>
2023-06-15 17:33:06 +0900

    [Gold III] Title: 크게 만들기, Time: 252 ms, Memory: 60904 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.py
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -3,10 +3,9 @@ num_arr = list(map(int, list(input())))
 stack = []
 
 for num in num_arr:
-    if k:
-        while k and stack and stack[-1] < num:
-            stack.pop()
-            k -= 1
+    while k and stack and stack[-1] < num:
+        stack.pop()
+        k -= 1
     stack.append(num)
 
 print("".join(map(str, stack[:len(stack) - k])))
