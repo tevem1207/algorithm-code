aff72e2c0f12359eed3f6c19f89786d70eff70f1
Taehee_Hwang <89463824+tevem1207@users.noreply.github.com>
2023-07-07 10:04:22 +0900

    [Gold III] Title: 팰린드롬 개수 구하기 (Small), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/14505. 팰린드롬 개수 구하기 （Small）/팰린드롬 개수 구하기 （Small）.py
@@ -0,0 +1,17 @@
+str = input()
+n = len(str)
+dp = [[0] * (n) for _ in range(n)]
+
+for i in range(n):
+    dp[i][i] = 1
+for i in range(n-1):
+    dp[i][i+1] = 3 if str[i] == str[i+1] else 2
+
+for i in range(2, n):
+    for j in range(n-1):
+        if j+i < n:
+            s, e = j, j+i
+            dp[s][e] = (dp[s+1][e] + dp[s][e-1]
+                        + (1 if str[s] == str[e] else -dp[s+1][e-1]))
+
+print(dp[0][n-1])
