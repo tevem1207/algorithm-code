46d409a766840cc8b8ba0a4fcfd0aa7977e413fc
Taehee_Hwang <89463824+tevem1207@users.noreply.github.com>
2025-02-20 17:17:32 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 168 ms, Memory: 11164 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
@@ -1,21 +1,14 @@
-const rl = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const input = require("fs").readFileSync(0, "utf-8").trim().split("\n");
+const numbers = input[1].split(" ").map(Number);
+const operators = input[2].split(" ").map(Number);
 
-const input = [];
 let min = Infinity;
 let max = -Infinity;
 
-rl.on("line", (line) => input.push(line)).on("close", () => {
-  const numbers = input[1].split(" ").map(Number);
-  const operators = input[2].split(" ").map(Number);
+dfs(numbers[0], 1, operators);
 
-  dfs(numbers[0], 1, operators);
-
-  console.log(max || 0);
-  console.log(min || 0);
-});
+console.log(max || 0);
+console.log(min || 0);
 
 function dfs(currentValue, index, operators) {
   if (operators.every((value) => value === 0)) {
@@ -24,32 +17,24 @@ function dfs(currentValue, index, operators) {
     return;
   }
 
-  const nextNum = parseInt(input[1].split(" ")[index]);
-
   if (operators[0] > 0) {
     operators[0]--;
-    dfs(currentValue + nextNum, index + 1, operators);
+    dfs(currentValue + numbers[index], index + 1, operators);
     operators[0]++;
   }
   if (operators[1] > 0) {
     operators[1]--;
-    dfs(currentValue - nextNum, index + 1, operators);
+    dfs(currentValue - numbers[index], index + 1, operators);
     operators[1]++;
   }
   if (operators[2] > 0) {
     operators[2]--;
-    dfs(currentValue * nextNum, index + 1, operators);
+    dfs(currentValue * numbers[index], index + 1, operators);
     operators[2]++;
   }
   if (operators[3] > 0) {
     operators[3]--;
-    dfs(
-      currentValue < 0
-        ? -Math.floor(-currentValue / nextNum)
-        : Math.floor(currentValue / nextNum),
-      index + 1,
-      operators
-    );
+    dfs(~~(currentValue / numbers[index]), index + 1, operators);
     operators[3]++;
   }
 }
